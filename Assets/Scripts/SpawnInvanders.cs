14c3d2fb5809e7e929cf8da3920c660f4d3b1e60
IagoFernandes1999 <72970480+IagoFernandes1999@users.noreply.github.com>
2021-03-29 16:13:56 +0100

    Initial commit

---
+++ Assets/Scripts/SpawnInvanders.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnInvanders : MonoBehaviour
+{
+    [SerializeField]
+    GameObject invasorA;
+
+    [SerializeField]
+    GameObject invasorB;
+
+    [SerializeField]
+    GameObject invasorC;
+
+    [SerializeField]
+    int nInvasores = 7;
+
+    [SerializeField]
+    float xMin = -3f;
+
+  
+
+    void Awake()
+    {
+        float x = xMin;
+        for (int i = 1; i <= nInvasores; i++ )
+        {
+             GameObject newInvader = Instantiate(invasorA, transform);
+             newInvader.transform.position = new Vector3(x, -1.5f, 0f);
+             x += 1f;
+        }
+        float x2 = xMin;
+        for (int i = 1; i <= nInvasores; i++)
+        {
+             GameObject newInvader = Instantiate(invasorA, transform);
+             newInvader.transform.position = new Vector3(x2, -1f, 0f);
+             x2 += 1f;
+        }
+        float x3 = xMin;
+        for (int i = 1; i <= nInvasores; i++)
+        {
+            GameObject newInvader = Instantiate(invasorB, transform);
+            newInvader.transform.position = new Vector3(x3, -0.5f, 0f);
+            x3 += 1f;
+        }
+        float x4 = xMin;
+        for (int i = 1; i <= nInvasores; i++)
+        {
+            GameObject newInvader = Instantiate(invasorB, transform);
+            newInvader.transform.position = new Vector3(x4, 0f, 0f);
+            x4 += 1f;
+        }
+        float x5 = xMin;
+        for (int i = 1; i <= nInvasores; i++)
+        {
+            GameObject newInvader = Instantiate(invasorC, transform);
+            newInvader.transform.position = new Vector3(x5, 0.5f, 0f);
+            x5 += 1f;
+        }
+
+
+
+
+
+    }
+
+
+
+}
